362f5c06b9aa79b4376a2389a0c8398a800052bd
Zhi Lin <zhilin.vivian.ooi@gmail.com>
2024-10-24 00:05:29 +0800

    updated check-gitignore regex logic

--- git-hooks/pre-commit/check-gitignore.sh
+++ git-hooks/pre-commit/check-gitignore.sh
@@ -5,8 +5,8 @@ export GIT_CONFIG_VALUE_0=""
 export GIT_CONFIG_KEY_1="user.email"
 export GIT_CONFIG_VALUE_1=""
 
-if git diff --merge-base main --name-only | grep -i '.gitignore'; then
-  echo '.gitignore changes are not allowed! Fixing...'
+if git diff --merge-base main --name-only --diff-filter=MD | grep -i '.gitignore'; then
+  echo 'Backend/.gitignore modifications or deletions are not allowed! Fixing...'
   git checkout origin/main Backend/.gitignore
   exit 0
 fi
