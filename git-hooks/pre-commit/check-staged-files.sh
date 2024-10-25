16769671c126955e442db43a29d837bfe93d7ef7
Zhi Lin <zhilin.vivian.ooi@gmail.com>
2024-10-25 10:17:04 +0800

    modifying the environments for pre-commit

--- git-hooks/pre-commit/check-staged-files.sh
+++ git-hooks/pre-commit/check-staged-files.sh
@@ -1,12 +1,11 @@
 #!sh
-#!/bin/sh
 # Check if .gitignore is modified and staged
 export GIT_CONFIG_KEY_0="user.name"
 export GIT_CONFIG_VALUE_0=""
 export GIT_CONFIG_KEY_1="user.email"
 export GIT_CONFIG_VALUE_1=""
 
-output=$(git diff --name-only --cached --diff-filter=AM | grep -P '^\.env(?!\.example$).*')
+output=$(git diff --name-only HEAD | grep -P '^\.env(?!\.example$).*')
 
 if [ -n "$output" ]; then
   echo "Do not include files with credentials. Violating file: $output"
