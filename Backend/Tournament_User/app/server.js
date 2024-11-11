deeb4c751ec0387166ec470c0018f9796e6388ce
arshiya8 <jahan.arshiya1@gmail.com>
2024-11-11 11:11:36 +0800

    cors origin added

--- Backend/Tournament_User/app/server.js
+++ Backend/Tournament_User/app/server.js
@@ -1,16 +1,21 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const tournamentUserRoutes = require('./routes/tournamentUserRoutes');
 const errorHandler = require('./middlewares/errorHandler');  // Import error-handling middleware
 const successHandler = require('./middlewares/successHandler');// Import success-handling middleware
-
+const corsMiddleware = require('./middlewares/corsMiddleware');
 
 // Middleware to parse incoming JSON
 app.use(express.json());
 
 // Tournament user routes
 app.use('/api', tournamentUserRoutes);
+
+// Apply custom CORS middleware
+app.use(corsMiddleware);
+
 
 
 // use the success-handling middleware
@@ -24,4 +29,5 @@ app.use(errorHandler);
 const PORT = process.env.DB_PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    console.log('Allowed Origins:', process.env.ALLOWED_ORIGINS?.split(',').map(origin => origin.trim()));
 });
